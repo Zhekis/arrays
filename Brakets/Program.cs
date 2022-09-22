a5e161ec850f639149894128b90e6197d6248e6b
Zheki <Zheki@ACER>
2022-09-22 23:54:24 +0500

    programm

--- Brakets/Program.cs
+++ Brakets/Program.cs
@@ -5,8 +5,6 @@ class Program
 
     static void Main(string[] args)
     {
-        int numberRow = 1;
-        int numberColumn = 0;
         int sumRow = 0;
         int multiplicationColumn = 1;
         int[,] array = { { 1, 3, 5, 6 }, { 2, 4, 6, 2 } };
@@ -15,15 +13,25 @@ class Program
         {
             for (int j = 0; j < array.GetLength(1); j++)
             {
-                if (j == numberColumn)
-                    multiplicationColumn = multiplicationColumn * array[i, j];
-                if (i == numberRow)
-                    sumRow += array[i, j];
-
                 Console.Write(array[i, j] + " ");
             }
+
             Console.WriteLine();
         }
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            multiplicationColumn = multiplicationColumn * array[i, 0];
+        }
+
+        for (int i = 1; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                    sumRow += array[i, j];
+            }
+        }
+
         Console.WriteLine();
         Console.WriteLine(sumRow);
         Console.WriteLine(multiplicationColumn);
