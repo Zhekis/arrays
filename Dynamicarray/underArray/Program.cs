79455b18e86afba688015d57cf7373a008164eb9
Zheki <Zheki@ACER>
2022-09-30 00:29:22 +0500

    fix final

--- Dynamicarray/underArray/Program.cs
+++ Dynamicarray/underArray/Program.cs
@@ -10,7 +10,41 @@ namespace underArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1");
+            int counter1 = 0;
+            int counterMax = 0;
+            int maxNumber = 0;
+            int[] array = new int[30];
+            Random random = new Random();
+            Console.WriteLine("Array:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 10);
+                Console.Write(array[i] + " ");
+            }
+
+            for (int i = 0; i < (array.Length - 1); i++)
+            {
+                if (array[i] == array[i + 1])
+                {
+                    counter1++;
+                }
+
+                if (counter1 > counterMax)
+                {
+                    maxNumber = array[i];
+                    counterMax = counter1;
+                }
+
+                if (array[i] != array[i + 1])
+                {
+                    counter1 = 1;
+                }    
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Число " + maxNumber + " повторяется большее число раз подряд (количество повторений " + counterMax + ").");
         }
     }
 }
