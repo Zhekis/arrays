caaeea298066a1cc54b8abb9089578bdeafa5f91
Zheki <Zheki@ACER>
2022-10-01 19:44:21 +0500

    1

---
+++ arrays/Split/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Hi my name is Zheka";
+            string[] words = phrase.Split(new char[] { ' ' });
+
+            foreach (string s in words)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
