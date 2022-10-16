756efe36fc06926b8c731e78675d5cb2b40b66d6
Zheki <Zheki@ACER>
2022-10-16 12:45:51 +0500

    shuffle

---
+++ Functions/Shuffle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 5, 6, 2, 3, 4, 7, 9 };
+
+            Shuffle(myArray);
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write(myArray[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Shuffle(int [] array)
+        {
+            Random random = new Random();
+
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
